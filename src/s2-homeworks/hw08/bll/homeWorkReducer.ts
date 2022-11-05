9a707e27fbef3d44bdeae4625be805935862c31e
Oleg <9helgi9gmail.com>
2022-11-05 21:29:09 +0100

    added hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,21 @@
 import {UserType} from '../HW8'
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
+        case 'sort': {
+            if (action.payload === 'up') {
+                return state.sort((a, b) => a.name > b.name ? 1 : -1)
+            }
+            if( action.payload === 'down'){
+                return state.sort((a, b) => a.name < b.name ? 1 : -1)
+            }
             return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age > action.payload)
         }
         default:
             return state
